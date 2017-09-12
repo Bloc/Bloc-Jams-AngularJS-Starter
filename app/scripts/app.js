3279f3b09ba7e515bb869af06c5ba4504cdb0130
James Capra <jcapra76@gmail.com>
2017-09-11 18:48:57 -0700

    Part 3, finished

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        
+        $stateProvider 
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+        
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+        
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
+            });
+    }
+
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
