7d7f4e8d10f693a1df8413dcc7ceab5215081fd5
James Capra <jcapra76@gmail.com>
2017-09-15 20:02:54 -0700

    Part 5 Controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,16 +9,19 @@
         $stateProvider 
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
         
             .state('album', {
                 url: '/album',
+                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
             })
         
             .state('collection', {
                 url: '/collection',
+                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
             });
     }
