5d31e99b321c9c1b7c5dc9bf130d5e8b1bd2d89f
James Capra <jcapra76@gmail.com>
2017-09-17 15:48:18 -0700

    Part 5 Controllers, the Assignment

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,6 +1,19 @@
 (function() {
     function(AlbumCtrl) {
-        this.albumData = albumPicasso;
+        this.albumData = {
+            title: 'The Colors',
+            artist: 'Pablo Picasso',
+            label: 'Cubism',
+            year: '1881',
+            albumArtUrl: 'assets/images/album_covers/11.png',
+            songs: [
+                { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+                { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+                { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+                { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+                { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+            ]
+        };
     }
 
     angular
