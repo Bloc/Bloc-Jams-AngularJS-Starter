76cb964657e79f992a3f284e7b4f90bba5aa97c8
Tim Dickeson II <imacubsfan23@gmail.com>
2017-09-25 13:58:49 -0500

    Checkpoint 3 routing and states Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJams', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      });
+  }
+
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
