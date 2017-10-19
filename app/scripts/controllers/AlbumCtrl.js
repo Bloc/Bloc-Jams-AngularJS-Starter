ef6a26c35462edf3cf1473c4e73f956491280f73
Kyle <ashcraft.k@husky.neu.edu>
2017-10-18 18:59:56 -0700

    added fixtures factory to handle album info distribution

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,13 @@
 (function(){
-  function AlbumCtrl(){
-    this.albumData = albumPicasso;
+  function AlbumCtrl(Fixtures){
+    this.albumData = Fixtures.getAlbum();
     this.songs = [];
-    for (var i = 0; i < albumPicasso.songs.length; i++){
+    for (var i = 0; i < this.albumData.songs.length; i++){
       this.songs.push(angular.copy(this.albumData.songs[i]))
     }
-    console.log(this.songs);
+
   }
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl)
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl])
 })();
