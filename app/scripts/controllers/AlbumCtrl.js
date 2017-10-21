95e09682fd868e987cb064a44764c161cde820d5
Matthew Driscoll <mattd5312@gmail.com>
2017-10-20 20:37:11 -0700

    assignment 5 attempt 2

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,9 @@
 (function () {
   function AlbumCtrl() {
     this.albumData = albumPicasso;
-    for (var i=0; i < 12; i++) {
-      this.albumData.push(angular.copy(albumPicasso));
-    }
   }
 
   angular
       .module('blocJams')
       .controller('AlbumCtrl', AlbumCtrl);
-})
+})();
