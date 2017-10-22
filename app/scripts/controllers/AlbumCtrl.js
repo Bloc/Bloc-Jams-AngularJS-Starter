9ef71f907f18a84b663fd8d6345c78375d8fccb5
Matthew Driscoll <mattd5312@gmail.com>
2017-10-22 15:39:39 -0700

    checkpoint 6 complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,9 @@
 (function () {
-  function AlbumCtrl() {
-    this.albumData = albumPicasso;
+  function AlbumCtrl(Fixtures) {
+    this.albumData = Fixtures.getAlbum();
   }
 
   angular
       .module('blocJams')
-      .controller('AlbumCtrl', AlbumCtrl);
+      .controller('AlbumCtrl', ['Fixtures',AlbumCtrl]);
 })();
