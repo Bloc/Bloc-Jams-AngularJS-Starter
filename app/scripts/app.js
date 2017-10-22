0152f76271cd48a0ece43d6730a52b1c5536ed4e
Robert Marsh <nhe6dc@gmail.com>
2017-10-22 15:47:28 -0400

    ctrl's up besides album

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,18 +6,20 @@
       requireBase: false
     });
     $stateProvider
-       .state('landing', {
-           url: '/',
-           templateUrl: '/templates/landing.html'
-       })
-      .state('album', {
-          url: '/album',
-          templateUrl: '/templates/album.html'
-      })
-      .state('collection', {
-        url: '/collection',
-        templateUrl: '/templates/collection.html'
-      });
+    .state('landing', {
+      url: '/',
+      controller: 'LandingCtrl as landing',
+      templateUrl: '/templates/landing.html'
+    })
+    .state('album', {
+      url: '/album',
+      templateUrl: '/templates/album.html'
+    })
+    .state('collection', {
+      url: '/collection',
+      controller: 'CollectionCtrl as collection',
+      templateUrl: '/templates/collection.html'
+    });
   }
 
   angular.module('blocJams', ['ui.router']);
