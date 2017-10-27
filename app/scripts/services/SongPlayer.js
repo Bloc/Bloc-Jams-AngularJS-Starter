56bbab6c976f9884d099dd42459c2af362968cd6
Matthew Driscoll <mattd5312@gmail.com>
2017-10-26 23:19:01 -0700

    assignment 7 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,12 +29,16 @@
             currentSong = song;
          };
 
+         var playSong = function(song) {
+           currentBuzzObject.play();
+           song.playing = true;
+         }
+
 
          SongPlayer.play = function(song) {
            if (currentSong !== song) {
             setSong(song);
-            currentBuzzObject.play();
-            song.playing = true;
+            playSong(song);
           } else if (currentSong === song) {
               if (currentBuzzObject.isPaused()) {
                 currentBuzzObject.play();
