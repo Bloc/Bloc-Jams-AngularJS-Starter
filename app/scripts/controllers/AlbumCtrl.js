0c13e6e09fd4a75415b3a748835f1271e58adfdf
Robert Marsh <nhe6dc@gmail.com>
2017-10-30 14:05:04 -0400

    cp 6 complete

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,10 @@
   (function() {
-  function AlbumCtrl() {
-  	this.albumData = albumPicasso;
-    
+       function AlbumCtrl(Fixtures) {
+  	 this.albumData = Fixtures.getAlbum();
+
   }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+       .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
