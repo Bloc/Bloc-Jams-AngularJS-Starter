6e7f782b573001fef41572da5de1ba39a6d12277
Mary Elizabeth Aleman <maryelizabethaleman@yahoo.com>
2017-11-03 14:26:08 -0500

    Completed checkpoint-4-template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,10 @@
          .state('album', {
              url: '/album',
              templateUrl: '/templates/album.html'
+         })
+         .state('collection', {
+             url: '/collection',
+             templateUrl: '/templates/collection.html',
          });
 
      }
