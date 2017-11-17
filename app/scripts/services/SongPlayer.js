f16fabf34782e3b25fb3c8d1d6929015b89d2be7
Mary Elizabeth Aleman <maryelizabethaleman@yahoo.com>
2017-11-17 11:18:56 -0600

    Completed checkpoint 7: services

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,53 @@
+(function() {
+     function SongPlayer() {
+          var SongPlayer = {};
+
+		 var currentSong = null;
+		 /**
+ 		* @desc Buzz object audio file
+		 * @type {Object}
+ 		*/    	 
+ 		var currentBuzzObject = null;
+
+			/**
+		* @function setSong
+ 		* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+ 		* @param {Object} song
+ 		* @returns {Number}
+ 		*/
+    	 var setSong = function(song) {
+    		if (currentBuzzObject) {
+        		currentBuzzObject.stop();
+        		currentSong.playing = null;
+    		}
+
+    		currentBuzzObject = new buzz.sound(song.audioUrl, {
+        	formats: ['mp3'],
+       		preload: true
+    		});
+    		currentSong = song;
+		};
+
+         SongPlayer.play = function(song) {
+         	if (currentSong !== song) {
+             	setSong(song);
+         		currentBuzzObject.play();   
+         		currentSong.playing = true;   
+            } else if (currentSong === song) {
+         	if (currentBuzzObject.isPaused()) {
+             currentBuzzObject.play();
+        	 }
+    		 }         
+     		};
+
+     	SongPlayer.pause = function(song) {
+     		currentBuzzObject.pause();
+     		song.playing = false;
+ 		};
+          
+          return SongPlayer;
+    		}
+     		angular
+         		.module('blocJams')
+         		.factory('SongPlayer', SongPlayer);
+ 			})();
