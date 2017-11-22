b787fbd94629df8c69f10d4acc1b421cde3cddd6
Mary Elizabeth Aleman <maryelizabethaleman@yahoo.com>
2017-11-21 20:22:10 -0600

    Completed assignment 9: directives

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -31,6 +31,10 @@
             	return {width: percentString()};
             };
 
+            scope.thumbStyle = function() {
+            	return {left: percentString()};
+            };
+
             scope.onClickSeekBar = function(event) {
              var percent = calculatePercent(seekBar, event);
              scope.value = percent * scope.max;
