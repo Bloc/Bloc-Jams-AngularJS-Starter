cc87a9d1f039460cb4d54664a0d02000e2095efd
Sanjana2000 <sanjana100@gmail.com>
2018-08-27 10:46:59 -0700

    CP-Controller completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,12 +9,18 @@
     $stateProvider
       .state('landing', {
         url: '/',
+        controller: 'LandingCtrl as landing',
         templateUrl: '/templates/landing.html'
       })
       .state('album', {
         url: '/album',
         templateUrl: '/templates/album.html'
-      });
+      })
+      .state('collection', {
+     url: '/collection',
+     controller: 'CollectionCtrl as collection',
+     templateUrl: '/templates/collection.html'
+ });
 
   }
 
