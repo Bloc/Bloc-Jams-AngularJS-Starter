5e4728f7f61e7286922085893481fb3ee4fa9a57
Sanjana2000 <sanjana100@gmail.com>
2018-08-30 06:41:46 -0700

    Creating and Injecting a factory service in application

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function() {
-  function AlbunCtrl() {
+  function AlbumCtrl(Fixtures) {
     this.albums = [];
     for (var i = 0; i < 12; i++) {
-      this.albums.push(angular.copy(albumPicasso));
+      //this.albums.push(angular.copy(albumPicasso));
+       this.albumData = Fixtures.getAlbum();
     }
   }
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl);
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
