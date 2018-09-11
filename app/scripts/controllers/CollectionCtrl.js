6e134720e1df5d035538eff873cdb089c4d7697b
Sanjana2000 <sanjana100@gmail.com>
2018-09-11 06:47:24 -0700

    CP-Services-Part2 completed

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,9 @@
 (function() {
     function CollectionCtrl(Fixtures) {
       this.albums = Fixtures.getCollection(12);
-  }
+    }
 
     angular
       .module('blocJams')
-      .controller('CollectionCtrl', ['Fixtures', CollectionCtrl);
+      .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
       })();
