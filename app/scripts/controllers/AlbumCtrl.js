7ea07159b5241019156a9fcd21dd4d83e1cf6592
Sanjana2000 <25292110+Sanjana2000@users.noreply.github.com>
2018-09-18 21:30:28 -0700

    Update AlbumCtrl.js

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,29 +1,21 @@
 (function() {
   function AlbumCtrl(Fixtures, SongPlayer) {
-<<<<<<< HEAD
     this.albums = [];
     for (var i = 0; i < 12; i++) {
-      //this.albums.push(angular.copy(albumPicasso));
+
       this.albumData = Fixtures.getAlbum();
       this.songPlayer = SongPlayer;
     }
-=======
-    //this.albums = [];
-    //for (var i = 0; i < 12; i++) {
-      //this.albums.push(angular.copy(albumPicasso));
-      this.albumData = Fixtures.getAlbum();
-      this.songPlayer = SongPlayer;
-    //}
->>>>>>> CP-Services-Part2
   };
 
   angular
     .module('blocJams')
-<<<<<<< HEAD
 
     .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
-=======
-    .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
-
->>>>>>> CP-Services-Part2
 })();
+
+
+
+
+
+
