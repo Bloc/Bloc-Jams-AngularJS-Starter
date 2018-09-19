c24e52b6e6545c074298f337e0a92f129e76fde8
Sanjana2000 <25292110+Sanjana2000@users.noreply.github.com>
2018-09-18 21:21:48 -0700

    Update SongPlayer.js

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,66 +1,89 @@
-(function() {
-  function SongPlayer() {
-    var SongPlayer = {};
-     var currentSong = null;
-     /*
-     * @desc Buzz object audio file
-     * @type {Object}
-     */
-    var currentBuzzObject = null;
-     /*
-     * @function setSong
-     * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-     * @param {Object} song
-     */
-    var setSong = function(song) {
-      if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        currentSong.playing = null;
-      }
-       currentBuzzObject = new buzz.sound(song.audioUrl, {
-        formats: ['mp3'],
-        preload: true
-      });
-       currentSong = song;
-    };
+function() {
+    function SongPlayer() {
+      var SongPlayer = {};
+
+      var currentSong = null;
+
+      /*
+       * @desc Buzz object audio file
+       * @type {Object}
+       */
+      var currentBuzzObject = null;
+
 
 
       /*
-    * @function playSong
-    * @desc Plays currently playing song.
-    * @param {Object} song
-    */
-var playSong = function(song) {
-  if (currentBuzzObject) {
-    currentBuzzObject.play();
-    currentSong.playing = true;
-};
+       * @function setSong
+       * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+       * @param {Object} song
+       */
+
+      var setSong = function(song) {
+        if (currentBuzzObject) {
+          currentBuzzObject.stop();
+          currentSong.playing = null;
+        }
+
+        currentBuzzObject = new buzz.sound(song.audioUrl, {
+          formats: ['mp3'],
+          preload: true
+        });
+
+        currentSong = song;
+
+      };
 
 
-
-/*
-* @function SongPlayer
-* @desc sets and plays the current Buzz object
-* @param song
-* @type object
-*/
-     SongPlayer.play = function(song) {
-      if (currentSong !== song) {
-        setSong(song);
-        playSong(song);
-      } else if (currentSong === song) {
-        if (currentBuzzObject.isPaused()) {
+      /*
+       * @function playSong
+       * @desc Plays currently playing song.
+       * @param {Object} song
+       */
+      var playSong = function(song) {
+        if (currentBuzzObject) {
           currentBuzzObject.play();
-         }
+          currentSong.playing = true;
+        };
+
+
+
+        /*
+         * @function SongPlayer
+         * @desc sets and plays the current Buzz object
+         * @param song
+         * @type object
+         */
+        SongPlayer.play = function(song) {
+          if (currentSong !== song) {
+            setSong(song);
+            currentBuzzObject.play();
+            song.playing = true;
+          } else if (currentSong === song) {
+            if (currentBuzzObject.isPaused()) {
+              currentBuzzObject.play();
+              song.playing = true;
+            }
+          }
+        };
+
+
+        SongPlayer.pause = function(song) {
+          currentBuzzObject.pause();
+          song.playing = false;
+        };
+
+        return SongPlayer;
       }
-    };
-     SongPlayer.pause = function(song) {
-      currentBuzzObject.pause();
-      song.playing = false;
-    };
-     return SongPlayer;
-  }
-   angular
-    .module('blocJams')
-    .factory('SongPlayer', SongPlayer);
- })();
+
+      angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+      }
+    })();
+
+
+
+
+
+
+
